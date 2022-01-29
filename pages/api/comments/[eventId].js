d8d82cff0cb899a7ad5c432e2218cbd6df4f4121
damorenon <damorenon@gmail.com>
2022-01-29 17:47:03 -0500

    Adding mongoDB error handling and rest of stuff

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,12 +1,22 @@
 // /api/comments/some-id
-import { MongoClient } from "mongodb";
+import {
+    connectDatabase,
+    insertDocument,
+    getAllDocuments
+} from "../../../helper/db-util";
 
 async function handler(req, res) {
     const eventId = req.query.eventId;
 
-    const client = await MongoClient.connect(
-        "mongodb+srv://damorenon:nqttbduy@cluster0.zmbzy.mongodb.net/events?retryWrites=true&w=majority"
-    );
+    let client;
+    try {
+        client = await connectDatabase();
+    } catch (error) {
+        res.status(500).json({
+            message: "Connecting to the database failed!"
+        });
+        return;
+    }
 
     if (req.method === "POST") {
         const { email, name, text } = req.body;
@@ -19,28 +29,34 @@ async function handler(req, res) {
             text.trim() === ""
         ) {
             res.status(422).json({ message: "invalid input" });
+            client.close();
             return;
         }
 
         const newComment = { eventId, email, name, text };
 
-        const db = client.db();
-        const result = await db.collection("comments").insertOne(newComment);
-
-        console.log(result);
-
-        res.status(201).json({
-            message: "Added comments.",
-            comment: newComment
-        });
+        let result;
+        try {
+            result = await insertDocument(client, "comments", newComment);
+            newComment._id = result.insertedId;
+            res.status(201).json({
+                message: "Added comments.",
+                comment: newComment
+            });
+        } catch (error) {
+            res.status(500).json({ message: "Inserting data failed!" });
+        }
     }
 
     if (req.method === "GET") {
-        const dummyList = [
-            { id: "c1", name: "Max", text: "A 1st comment" },
-            { id: "c2", name: "Manuel", text: "A 2nd comment" }
-        ];
-        res.status(201).json({ comments: dummyList });
+        try {
+            const documents = await getAllDocuments(client, "comments", {
+                _id: -1
+            });
+            res.status(201).json({ comments: documents });
+        } catch (error) {
+            res.status(500).json({ message: "Getting comments failed." });
+        }
     }
 
     client.close();
