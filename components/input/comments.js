43a1ff4dfbba22c520b9a2e21b686fe78e4f809c
damorenon <damorenon@gmail.com>
2022-02-07 17:25:37 -0500

    Adding react Context for notifications in async requests

--- components/input/comments.js
+++ components/input/comments.js
@@ -1,19 +1,25 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 
 import CommentList from "./comment-list";
 import NewComment from "./new-comment";
 import classes from "./comments.module.css";
+import NotificationContext from "../../store/notification-context";
 
 function Comments({ eventId }) {
     const [showComments, setShowComments] = useState(false);
     const [comments, setComments] = useState([]);
+    const [isFetchingComments, setIsFetchingComments] = useState(false);
+
+    const notificationCtx = useContext(NotificationContext);
 
     useEffect(() => {
         if (showComments) {
+            setIsFetchingComments(true);
             fetch("/api/comments/" + eventId)
                 .then((response) => response.json())
                 .then((data) => {
                     setComments(data.comments);
+                    setIsFetchingComments(false);
                 });
         }
     }, [eventId, showComments]);
@@ -23,6 +29,12 @@ function Comments({ eventId }) {
     }
 
     function addCommentHandler(commentData) {
+        notificationCtx.showNotification({
+            title: "Sending comment...",
+            message: "Your comment is currently beign stored into a database",
+            status: "pending"
+        });
+
         fetch("/api/comments/" + eventId, {
             method: "POST",
             body: JSON.stringify(commentData),
@@ -30,8 +42,30 @@ function Comments({ eventId }) {
                 "Content-Type": "application/json"
             }
         })
-            .then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                response.json().then((data) => {
+                    throw new Error(data.message || "Somethig went wrong!");
+                });
+            })
+            .then(() =>
+                notificationCtx.showNotification({
+                    title: "Success!",
+                    message: "Your comment was saved!",
+                    status: "success"
+                })
+            )
+            .catch((error) => {
+                notificationCtx.showNotification({
+                    title: "Error!",
+                    message:
+                        error.message ||
+                        "Error when trying to register your comment",
+                    status: "error"
+                });
+            });
     }
 
     return (
@@ -40,7 +74,10 @@ function Comments({ eventId }) {
                 {showComments ? "Hide" : "Show"} Comments
             </button>
             {showComments && <NewComment onAddComment={addCommentHandler} />}
-            {showComments && <CommentList items={comments} />}
+            {showComments && !isFetchingComments && (
+                <CommentList items={comments} />
+            )}
+            {showComments && isFetchingComments && <p>Loading...</p>}
         </section>
     );
 }
--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,22 +1,51 @@
-import { useRef } from "react";
-import useSWR from "swr";
+import { useRef, useContext } from "react";
+import NotificationContext from "../../store/notification-context";
 import classes from "./newsletter-registration.module.css";
 
 function NewsletterRegistration() {
     const emailInputRef = useRef();
+    const notificationCtx = useContext(NotificationContext);
 
     function registrationHandler(event) {
         event.preventDefault();
 
         const enteredEmail = emailInputRef.current.value;
 
+        notificationCtx.showNotification({
+            title: "Signing up...",
+            message: "Registering for newsletter",
+            status: "pending"
+        });
+
         fetch("/api/newsletter", {
             method: "POST",
             body: JSON.stringify({ email: enteredEmail }),
             headers: { "Content-Type": "application/json" }
         })
-            .then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                response.json().then((data) => {
+                    throw new Error(data.message || "Somethig went wrong!");
+                });
+            })
+            .then(() =>
+                notificationCtx.showNotification({
+                    title: "Success!",
+                    message: "Succeesully registered for newsletter!",
+                    status: "success"
+                })
+            )
+            .catch((error) => {
+                notificationCtx.showNotification({
+                    title: "Error!",
+                    message:
+                        error.message ||
+                        "Error when Registering for newsletter",
+                    status: "error"
+                });
+            });
     }
 
     return (
